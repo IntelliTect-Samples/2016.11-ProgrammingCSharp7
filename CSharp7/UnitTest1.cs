20aede4ca6a3276cde267dd23c9ecc14ea806398
mark <mark@DESKTOP-IEHGDNQ>
2016-09-28 07:51:26 -0700

    Add project files.

---
+++ CSharp7/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp7
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
