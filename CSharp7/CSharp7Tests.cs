cf69b83c59ba5e128810c520de3e5f4222e183ab
Mark Michaelis <mark@IntelliTect.com>
2016-11-15 15:40:03 -0800

    Updated to latest references. Addressed all compiler warnings.

--- CSharp7/CSharp7Tests.cs
+++ CSharp7/CSharp7Tests.cs
@@ -9,7 +9,7 @@ namespace CSharp7
     public class CSharp7Tests
     {
         // Confirm C# 7.0 is active using a binary literal
-        long LargestSquareNumberUsingAllDigits = 
+        public long LargestSquareNumberUsingAllDigits = 
             0b0010_0100_1000_1111_0110_1101_1100_0010_0100;  // 9,814,072,356
         // Confirm C# 7.0 is active digit separator
         long MaxInt64 { get; } = 9_223_372_036_854_775_807;  // Equivalent to long.MaxValue
@@ -60,67 +60,9 @@ namespace CSharp7
         #endregion // Out Parameter Declaration
 
 
-        #region Tuples
-        [TestMethod][Ignore]
-        public void ReturnTupleWithNamedProperties()
-        {
-
-            (double Latitude, double Longitude) GetCurrentCoordinate(string address = null)
-            {
-                if (address is null)
-                {
-                    //  "10 Downing Street, London, United Kingdom"
-                    return (51.50336335364312, -0.12762486934661865);
-                }
-                else
-                {
-                    // Lookup look up current location 
-                    // ...
-                    return (16.766123, -3.004031); // Assumes Timbuktu
-                }
-            }
-
-            (double Latitude, double Longitude) expected =
-                (51.50336335364312, -0.12762486934661865);
-
-            (double Latitude, double Longitude) actual = 
-                GetCurrentCoordinate("10 Downing Street, London, United Kingdom");
-            Assert.AreEqual<(double Latitude, double Longitude)>(
-                expected, actual);
-            Assert.AreEqual<double>(51.50336335364312, actual.Latitude);
-            Assert.AreEqual<double>(51.50336335364312, actual.Latitude);
-
-            // WARNING: The tuple element name 'Lat' is ignored because
-            // a different name is specified by the target type
-            // '(double Latitude, double Longitude).
-            expected = (Lat: 16.766123, Long: -3.004031);
-
-            Assert.AreEqual<(double Latitude, double Longitude)>(
-                expected, GetCurrentCoordinate());
-            
-            Assert.AreEqual<(double Latitude, double Longitude)>(
-                (Lat: 16.766123, Long: -3.004031), GetCurrentCoordinate());
-
-            
-            //(double, double) GetSampleCoordinate() => (Lat: 16.766123, Long: -3.004031);
-
-            // Not surpisingly, the same function can't be declared twice
-            (double Latitude, double Longitude) GetSampleCoordinate(string address=null)
-            {
-                // In functions, the named properties are ignored.
-                (double Latitude, double Longitude) result = 
-                    address==null?(Latitude: 16.766123, Longitude: -3.004031) : // warning CS8123: The tuple element name 'Longitude' is ignored because a different name is specified by the target type '(double, double)'.
-                    (Lat: 16.766123, Long: -3.004031); // warning CS8123: The tuple element name 'Long' is ignored because a different name is specified by the target type '(double, double)'.
-
-                return result;
-            }
-            GetSampleCoordinate();
-        }
-        #endregion // Tuples
-
         System.Threading.Tasks.ValueTask<T> Blah<T>()
         {
-            return null;
+            return default(System.Threading.Tasks.ValueTask<T>);
         }
 
         [TestMethod]
