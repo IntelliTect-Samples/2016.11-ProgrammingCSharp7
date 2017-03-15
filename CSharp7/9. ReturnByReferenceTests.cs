b64d7a0716f6624261062d1fbf4d4ff458a1cb4e
Mark Michaelis <mark@IntelliTect.com>
2017-03-15 10:09:26 -0700

    Reorganized ReturnByReferenceTests.

--- CSharp7/9. ReturnByReferenceTests.cs
+++ CSharp7/9. ReturnByReferenceTests.cs
@@ -7,60 +7,6 @@ namespace CSharp7
     [TestClass]
     public class ReturnByReferenceTests
     {
-        [TestMethod]
-        public void ByRefCompileErrors()
-        {
-            void AssertStatementsFailCompilation(string body, params string[] expectedText)
-            {
-                string MethodScaffolding = $@"#pragma warning disable CS0168 // Disable variable declared but never used.
-                {body}
-                #pragma warning restore CS0168";
-                CompilerAssert.StatementsFailCompilation(MethodScaffolding, expectedText);
-
-            }
-            AssertStatementsFailCompilation("ref int number;",
-                "Error CS8174: A declaration of a by-reference variable must have an initializer");
-            AssertStatementsFailCompilation("ref int number = ref 42;",
-                "Error CS1510: A ref or out value must be an assignable variable");
-            AssertStatementsFailCompilation("ref int number = null;",
-                "Error CS1510: A ref or out value must be an assignable variable",
-                "Error CS8172: Cannot initialize a by-reference variable with a value");
-            AssertStatementsFailCompilation("ref string number = \"Inigo Montoya\";",
-                "Error CS1510: A ref or out value must be an assignable variable",
-                "Error CS8172: Cannot initialize a by-reference variable with a value");
-            AssertStatementsFailCompilation(
-                @"Guid guid = Guid.NewGuid();
-                 ref object obj = ref guid;",
-                "Error CS8173: The expression must be of type 'object' because it is being assigned by reference");
-            AssertStatementsFailCompilation(
-                @"Guid guid = Guid.NewGuid();
-                 ref object obj = ref guid;",
-                "Error CS8173: The expression must be of type 'object' because it is being assigned by reference");
-            AssertStatementsFailCompilation(
-                @"IEnumerable<ref int> numbers;",
-                "Error CS1073: Unexpected token 'ref'");
-            //AssertStatementsFailCompilation(
-            //    @"ref int[] numbers; ",
-            //    "Error CS1073: Unexpected token 'ref'");
-        }
-        class SampleObject1
-        {
-            public static string _Text = "Inigo Montoya";
-            public ref string Text { get { return ref _Text; } }
-            public ref string Method1() => ref Text;
-        }
-        class SampleObject2
-        {
-            ref string Method1()
-            {
-                SampleObject1 sampleObject = new SampleObject1();
-                return ref sampleObject.Text;
-            }
-        }
-
-        //byte[] _Image;
-        //public ref byte[] Image { get {  return ref _Image; } }
-
         static public ref byte FindFirstRedEyePixel(byte[] image)
         {
             //// Do fancy image detection perhaps with machine learning
@@ -122,6 +68,60 @@ namespace CSharp7
             }
             throw new InvalidOperationException($"{nameof(Thing.A)} is already initialized.");
         }
+
+        [TestMethod]
+        public void ByRefCompileErrors()
+        {
+            void AssertStatementsFailCompilation(string body, params string[] expectedText)
+            {
+                string MethodScaffolding = $@"#pragma warning disable CS0168 // Disable variable declared but never used.
+                {body}
+                #pragma warning restore CS0168";
+                CompilerAssert.StatementsFailCompilation(MethodScaffolding, expectedText);
+
+            }
+            AssertStatementsFailCompilation("ref int number;",
+                "Error CS8174: A declaration of a by-reference variable must have an initializer");
+            AssertStatementsFailCompilation("ref int number = ref 42;",
+                "Error CS1510: A ref or out value must be an assignable variable");
+            AssertStatementsFailCompilation("ref int number = null;",
+                "Error CS1510: A ref or out value must be an assignable variable",
+                "Error CS8172: Cannot initialize a by-reference variable with a value");
+            AssertStatementsFailCompilation("ref string number = \"Inigo Montoya\";",
+                "Error CS1510: A ref or out value must be an assignable variable",
+                "Error CS8172: Cannot initialize a by-reference variable with a value");
+            AssertStatementsFailCompilation(
+                @"Guid guid = Guid.NewGuid();
+                 ref object obj = ref guid;",
+                "Error CS8173: The expression must be of type 'object' because it is being assigned by reference");
+            AssertStatementsFailCompilation(
+                @"Guid guid = Guid.NewGuid();
+                 ref object obj = ref guid;",
+                "Error CS8173: The expression must be of type 'object' because it is being assigned by reference");
+            AssertStatementsFailCompilation(
+                @"IEnumerable<ref int> numbers;",
+                "Error CS1073: Unexpected token 'ref'");
+            //AssertStatementsFailCompilation(
+            //    @"ref int[] numbers; ",
+            //    "Error CS1073: Unexpected token 'ref'");
+        }
+        class SampleObject1
+        {
+            public static string _Text = "Inigo Montoya";
+            public ref string Text { get { return ref _Text; } }
+            public ref string Method1() => ref Text;
+        }
+        class SampleObject2
+        {
+            ref string Method1()
+            {
+                SampleObject1 sampleObject = new SampleObject1();
+                return ref sampleObject.Text;
+            }
+        }
+
+        //byte[] _Image;
+        //public ref byte[] Image { get {  return ref _Image; } }
 
     }
 }
